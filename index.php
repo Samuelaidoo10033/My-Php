df31e9d131a5e226bea9c7593042060a89dc04b5
Samuel Aidoo <samuelaidoo10033@gmail.com>
2020-09-17 20:23:42 +0000

    MyfirstPhpCode

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php 
+// Variable
+/*$greeting = "Hello";
+$target = "World";
+$phrase = $greeting . "" . $target;
+echo $phrase;
+?>
+<br /> 
+<?php
+echo "$phrase Again<br / >";
+echo "{$phrase}Again";
+
+
+$first = "This is a quick brown fox";
+$second = "Jumped over the lazy dog";
+$third = $first;
+$third .= $second;
+echo $third;
+?>
+
+<br /> 
+Lowercase: <?php echo strtolower($third); ?><br />
+Uppercase: <?php echo strtoupper($third); ?><br />
+Uppercase first: <?php echo ucfirst($third); ?><br />
+Uppercase word: <?php echo ucwords($third); ?><br />
+
+
+$var1 = 3;
+$var2 = 4;
+?>
+Basic Math: <?php echo ((1 + 2 + $var1) * $var2 / 2 - 5); ?> <br />
+Absolute Value: <?php echo abs(0 - 300);
+?>
+
+
+
+echo $float = 3.14; ?> <br />
+
+Round: <?php echo round($float); ?> <br /> 
+  
+
+//Array
+
+$numbers = array(4,5,6,7,8,5);
+echo $numbers[1];
+?>
+<?php $mixed = array(6, "Fox", "dog", array("x", "y", "z")); ?>
+<?php echo $mixed[2];?> <br /> 
+<?php echo $mixed[3]; ?> <br />
+
+//Associative Array
+
+$assoc = array("first_name" => "Kelvin" , "last_name" => "Samuel"); 
+
+echo $assoc ["first_name"] . "" . $assoc ["last_name"];?> 
+*/
+
+$Name = array("Samuel" , "Aidoo", "Millicent" , "Tilly");
+echo $Name[2];
+?>
+
